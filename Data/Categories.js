7f7bdd4247810b5630ec2f9e8c8368546e1170aa
eric <eric@example.com>
2025-11-22 16:22:03 +0800

    Design basic interface

---
+++ Data/Categories.js
@@ -0,0 +1,10 @@
+const categories = [
+      { name: "English Tutors", link: "english.html" },
+      { name: "Spanish Tutors", link: "spanish.html" },
+      { name: "Chinese Tutors", link: "chinese.html" },
+      { name: "Japanese Tutors", link: "japanese.html" },
+      { name: "Korean Tutors", link: "korean.html" },
+      { name: "French Tutors", link: "french.html" },
+      { name: "Arabic Tutors", link: "arabic.html"},
+      { name: "German Tutors", link: "germany.html"}
+    ];
